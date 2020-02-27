5508ec13683c7ea060825397decd24695aae9011
Reed Nelson-Saunders <reedns@hotmail.com>
2020-02-26 16:24:42 -0800

    create loan calculator

---
+++ lesson_2/loan_calculator/loan_calculator.rb
@@ -0,0 +1,45 @@
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def calculate(amount, apr, years)
+  mpr = apr / 12 / 100
+  months = years * 12
+
+  payment = amount * (mpr / (1 - (1 + mpr)**(-months)))
+  payment.round(2)
+end
+
+def valid_number?(num)
+  Float(num)&.> 0
+rescue
+  false
+end
+
+def format_input(input)
+  input.gsub(/\$|,|%/, '')
+end
+
+def input_loop(msg)
+  input = ''
+  loop do
+    prompt(msg)
+    input = format_input(gets.chomp)
+    if valid_number?(input)
+      break
+    else
+      prompt("Must enter a valid positive number. #{msg}")
+    end
+  end
+  input.to_f
+end
+
+prompt('Welcome to the Loan Calculator')
+
+amount = input_loop('Please enter the loan amount')
+apr = input_loop('Please enter the annual percentage rate (APR)')
+years = input_loop('Please enter loan duration in years')
+
+result = calculate(amount, apr, years)
+
+prompt("The monthly payment for this loan is #{result}")
